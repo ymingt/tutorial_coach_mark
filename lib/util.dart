349436fb97a80c390f0fc10ee64ffc136af147cb
Tan Yoke Ming <yokeming.tan@ifastfinancial.com>
2020-05-21 15:05:48 +0800

    Change: added way to find RenderBox child of RenderSliver.

--- lib/util.dart
+++ lib/util.dart
@@ -2,18 +2,29 @@ import 'package:flutter/widgets.dart';
 import 'package:tutorial_coach_mark/target_focus.dart';
 import 'package:tutorial_coach_mark/target_position.dart';
 
+RenderBox _getChildRenderObject(RenderObject renderObject) {
+  RenderBox renderBox;
+  renderObject.visitChildren((child) {
+    renderBox = child;
+  });
+  return renderBox;
+}
+
 TargetPosition getTargetCurrent(TargetFocus target) {
   if (target.keyTarget != null) {
     var key = target.keyTarget;
 
     try {
-      final RenderBox renderBoxRed = key.currentContext.findRenderObject();
+      final RenderObject renderObject = key.currentContext.findRenderObject();
+      final RenderBox renderBoxRed = renderObject is RenderBox
+          ? renderObject
+          : _getChildRenderObject(renderObject);
       final size = renderBoxRed.size;
       final offset = renderBoxRed.localToGlobal(Offset.zero);
 
       return TargetPosition(size, offset);
     } catch (e) {
-      print("ERROR: Não foi possivem oter informações da KEY");
+      print("ERROR: Failed to get render box of key's current info");
       return null;
     }
   } else {
